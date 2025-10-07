94a2dcbbec013092384b6cbe71d5ba997e13600b
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-10-07 05:10:55 -0500

    finished

---
+++ chamber/scripts/join.js
@@ -0,0 +1,19 @@
+//Guardar fecha y hora exacta cada que envias o abras un formulario.
+document.querySelector("#timestamp").value = new Date().toLocaleString();
+
+// 🎬 Lógica para abrir y cerrar modales
+const openButtons = document.querySelectorAll(".learn-btn");
+const closeButtons = document.querySelectorAll(".close-btn");
+
+openButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modalId = button.getAttribute("data-modal");
+    document.getElementById(modalId).showModal();
+  });
+});
+
+closeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    button.closest("dialog").close();
+  });
+});
