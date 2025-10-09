84feae5e2dfb62a5a248bff4e52435e3c18f2bee
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-10-09 01:30:06 -0500

    finished

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,6 +1,16 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
+// Botón hamburguesa
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+});
+
+
 //Guardar fecha y hora exacta cada que envias o abras un formulario.
 document.querySelector("#timestamp").value = new Date().toLocaleString();
 
